af1ea29b3a22f72efb5518c0176ee2919b9557de
Евгений Жиров <girov189916@gmail.com>
2021-02-10 22:38:52 +0300

    last

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,10 @@
+import data from '../data.json'
+
 export function getData() {
-  return fetch('opendata/7705851331-stat_library/data-2016-11-10T00-00-00-structure-2016-09-12T00-00-00.json')
+  return data
+  //fetch('opendata/7705851331-stat_library/data-2016-11-10T00-00-00-structure-2016-09-12T00-00-00.json')
   //fetch('https://data.gov.ru/sites/default/files/opendata/7705851331-stat_library/data-2016-11-10T00-00-00-structure-2016-09-12T00-00-00.json')
-  .then(response => response.json())
+  //.then(response => response.json())
 }
 
 
